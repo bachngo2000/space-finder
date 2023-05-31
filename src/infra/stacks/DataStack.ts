5b0abf0c130f1c3c86c1293115b1221c2026e733
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-05-31 12:44:20 -0500

    created an empty DataStack stack

--- src/infra/stacks/DataStack.ts
+++ src/infra/stacks/DataStack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
+// created an empty stack
 export class DataStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props)
