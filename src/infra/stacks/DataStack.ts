de71bd46e922f7ed68848b4b8b6927ac304c19b4
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-06-01 12:42:18 -0500

    prepared table for export

--- src/infra/stacks/DataStack.ts
+++ src/infra/stacks/DataStack.ts
@@ -1,16 +1,19 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { AttributeType, ITable, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 import { getSuffixFromStack } from '../Utils';
 
 // created an empty stack
 export class DataStack extends Stack {
+
+    public readonly spacesTable: ITable
+
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
         const suffix = getSuffixFromStack(this);
 
-        new Table(this, 'SpacesTable', {
+        this.spacesTable = new Table(this, 'SpacesTable', {
             partitionKey: {
                 name: 'id',
                 type: AttributeType.STRING
