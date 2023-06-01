fe9f86ad006456b6950928529522bbc4e6867245
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-06-01 11:31:19 -0500

    export HelloLambdaIntegration from our LambdaStack

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -1,19 +1,25 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import { join } from 'path';
 
 // created a LambdaStack stack that consists of multiple lambdas
 export class LambdaStack extends Stack {
+
+    public readonly helloLambdaIntegration: LambdaIntegration
+
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props)
 
         //create a new Lambda
         // 'handler' specifies the code that will be executed inside the lambda
-        new LambdaFunction(this, 'HelloLambda', {
+        const helloLambda = new LambdaFunction(this, 'HelloLambda', {
             runtime: Runtime.NODEJS_18_X,
             handler: 'hello.main',
             code: Code.fromAsset(join(__dirname, '..','..', 'services'))
         })
+
+        this.helloLambdaIntegration = new LambdaIntegration(helloLambda)
     }
 }
