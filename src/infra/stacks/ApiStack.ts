7c9664f8eaaaacfde8249d6783938f8b6b881aa9
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-06-01 12:13:42 -0500

    modified the ApiStack

--- src/infra/stacks/ApiStack.ts
+++ src/infra/stacks/ApiStack.ts
@@ -12,8 +12,12 @@ export class ApiStack extends Stack {
         super(scope, id, props);
 
         // created a construct
+        // const api belongs to this and has an id of SpacesApi
         const api = new RestApi(this, 'SpacesApi');
+        // add a resource by hand
         const spacesResource = api.root.addResource('spaces');
+        // add a simple GET method.
+        // here we need an integration object because our lambda is in a different stack. We need to export that from the  Lambdastack and import it in. So inside our API stack, this means that we need to extend our properties and this means that we need to make these two stacks communicate with each other.
         spacesResource.addMethod('GET', props.helloLambdaIntegration);
     }
 }
