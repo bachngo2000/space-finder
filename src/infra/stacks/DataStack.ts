6eb3e99759e7ae3a25082683dcb8ec506c321949
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-06-01 12:39:52 -0500

    added a dynamoDB table

--- src/infra/stacks/DataStack.ts
+++ src/infra/stacks/DataStack.ts
@@ -1,9 +1,21 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
+import { getSuffixFromStack } from '../Utils';
 
 // created an empty stack
 export class DataStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
-        super(scope, id, props)
+        super(scope, id, props);
+
+        const suffix = getSuffixFromStack(this);
+
+        new Table(this, 'SpacesTable', {
+            partitionKey: {
+                name: 'id',
+                type: AttributeType.STRING
+            },
+            tableName: `SpaceStack-${suffix}`
+        })
     }
 }
