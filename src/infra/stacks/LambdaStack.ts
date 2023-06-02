a2bf0f44b6b26683798601c6e09941fe2a915efd
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-06-02 11:53:26 -0500

    testing AWS SDK

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 import { ITable } from 'aws-cdk-lib/aws-dynamodb';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
@@ -37,7 +38,16 @@ export class LambdaStack extends Stack {
             environment: {
                 TABLE_NAME: props.spacesTable.tableName
             }
-        })
+        });
+
+        helloLambda.addToRolePolicy(new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: [
+                's3:ListAllMyBuckets',
+                's3:ListBucket'
+            ],
+            resources: ["*"]
+        }))
 
 
         this.helloLambdaIntegration = new LambdaIntegration(helloLambda)
--- src/services/hello.ts
+++ src/services/hello.ts
@@ -1,13 +1,21 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
 import { v4 } from 'uuid'
+import { S3Client, ListBucketsCommand} from '@aws-sdk/client-s3'
+
+const s3Client = new S3Client({});
 
 // the handler function receives an event and a context
 // if this lambda is accessed by APIGateWay, then APIGatewayProxyEvent is the type of our event
 async function handler(event: APIGatewayProxyEvent, context: Context) {
+
+    const command = new ListBucketsCommand({});
+
+    const listBucketResult = (await s3Client.send(command)).Buckets;
+
     // response of type APIGatewayProxyResult
     const response: APIGatewayProxyResult = {
         statusCode: 200,
-        body: JSON.stringify('Hello from lambda using TypeScript!' + v4())
+        body: JSON.stringify('Hello from lambda, here are your buckets:' + JSON.stringify(listBucketResult))
     }
 
     // print the event
