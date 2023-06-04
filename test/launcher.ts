54ae607d56f041127d9116a7ee4e65ed2bab45df
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-06-04 18:45:57 -0500

    testing queries

--- test/launcher.ts
+++ test/launcher.ts
@@ -1,3 +1,11 @@
-import { handler } from "../src/services/hello";
+import { handler } from "../src/services/spaces/handler"; 
 
-handler({} as any, {} as any);
+process.env.AWS_REGION = "us-east-2";
+process.env.TABLE_NAME = "SpaceTable-062cd2af5341";
+
+handler({
+    httpMethod: 'POST',
+    body: JSON.stringify({
+        location: 'Dallas'
+    })
+} as any, {} as any);
