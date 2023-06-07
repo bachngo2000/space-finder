8eb6687f04eaf15a5dbb427ca1f6a6e26086040b
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-06-07 14:20:24 -0500

    add s3 bucket to hold photo to the DataStack

--- src/infra/stacks/DataStack.ts
+++ src/infra/stacks/DataStack.ts
@@ -1,20 +1,56 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import { AttributeType, ITable, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 import { getSuffixFromStack } from '../Utils';
+import { Bucket, BucketAccessControl, HttpMethods, IBucket, ObjectOwnership } from 'aws-cdk-lib/aws-s3';
 
 // created an Data stack that contains a Dynamo DB table
 export class DataStack extends Stack {
 
     // ITable is an interface that represents a DynamoDB Table
     // We do this so we can reference the table in other stacks
-    public readonly spacesTable: ITable
+    public readonly spacesTable: ITable;
+    public readonly deploymentBucket: IBucket;
+    public readonly photosBucket: IBucket;
+    
 
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
         // getting suffix from the stack to form the db table's name
         const suffix = getSuffixFromStack(this);
+
+        this.deploymentBucket = new Bucket(this, 'SpaceFinderFrontend', {
+            bucketName: `space-finder-frontend-${suffix}`,
+            publicReadAccess: true,
+            websiteIndexDocument: 'index.html'
+        })
+
+        this.photosBucket = new Bucket(this, 'SpaceFinderPhotos', {
+            bucketName: `space-finder-photos-${suffix}`,
+            cors: [{
+                allowedMethods: [
+                    HttpMethods.HEAD,
+                    HttpMethods.GET,
+                    HttpMethods.PUT
+                ],
+                allowedOrigins: ['*'],
+                allowedHeaders: ['*']
+            }],
+            //accessControl: BucketAccessControl.PUBLIC_READ //currently not working
+            objectOwnership: ObjectOwnership.OBJECT_WRITER,
+            // this way, our bucket will be able to hold public files
+            blockPublicAccess: {
+                blockPublicAcls: false,
+                blockPublicPolicy: false,
+                ignorePublicAcls: false,
+                restrictPublicBuckets: false
+            }
+        });
+        
+        new CfnOutput(this, 'SpaceFinderPhotosBucketName', {
+            value: this.photosBucket.bucketName
+        });
 
         this.spacesTable = new Table(this, 'SpacesTable', {
             partitionKey: {
