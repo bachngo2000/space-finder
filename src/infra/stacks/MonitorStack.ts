563e08b83475f8409b404c4bb4d1229502052f47
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-07-02 23:43:38 -0400

    Built and deployed an AWS CloudWatch Alarm as the MonitorStack using CDK

--- src/infra/stacks/MonitorStack.ts
+++ src/infra/stacks/MonitorStack.ts
@@ -1,4 +1,5 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import { Alarm, Metric, Unit } from "aws-cdk-lib/aws-cloudwatch";
 import { Construct } from "constructs";
 
 //created a new stack, a monitoring stack for our AWS CloudWatch alarm
@@ -6,5 +7,22 @@ export class MonitorStack extends Stack {
     
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
+
+        const spacesApi4xxAlarm = new Alarm(this, 'spacesApi4xxAlarm', {
+            //specify a new metric
+            metric: new Metric({
+                metricName: '4XXError',
+                namespace: 'AWS/ApiGateway',
+                period: Duration.minutes(1),
+                statistic: 'Sum',
+                unit: Unit.COUNT,
+                dimensionsMap: {
+                    "ApiName": "SpacesApi"
+                }
+            }),
+            evaluationPeriods: 1,
+            threshold: 5,
+            alarmName: 'SpacesApi4xxAlarm'
+        })
     }
 }
