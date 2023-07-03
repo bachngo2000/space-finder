7cef0449f7751b79c0d9fec5e764f617cdd66c93
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-07-03 12:32:24 -0400

    enable tracing in our spacesLambda

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -1,8 +1,8 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 import { ITable } from 'aws-cdk-lib/aws-dynamodb';
 import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
-import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Code, Function as LambdaFunction, Runtime, Tracing } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 import { join } from 'path';
@@ -59,7 +59,10 @@ export class LambdaStack extends Stack {
             entry: (join(__dirname, '..','..', 'services', 'spaces', 'handler.ts')),
             environment: {
                 TABLE_NAME: props.spacesTable.tableName
-            }
+            },
+            //enable tracing in our spacesLambda
+            tracing: Tracing.ACTIVE,
+            timeout: Duration.minutes(1)
         });
 
         spacesLambda.addToRolePolicy(new PolicyStatement({
